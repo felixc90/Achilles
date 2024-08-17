cbca7568db0b9f230312b38cef9c997d136a63d1
felixcao <felixcao37@gmail.com>
2024-08-17 13:12:20 +1000

    set up basic command

--- bot/commands/graph.ts
+++ bot/commands/graph.ts
@@ -1,29 +1,30 @@
-import { ActionRowBuilder, CommandInteraction, SlashCommandBuilder, SlashCommandUserOption, UserSelectMenuBuilder } from "discord.js";
+import { ActionRowBuilder, CommandInteraction, SlashCommandBuilder, SlashCommandIntegerOption, SlashCommandStringOption, SlashCommandUserOption, UserSelectMenuBuilder } from "discord.js";
 import { errorMessage } from "../utils/error";
 
 export const data = new SlashCommandBuilder()
   .setName("graph")
-  .setDescription("Create a new graph displaying run history!")
-	.addUserOption(
-		new SlashCommandUserOption()
-			.setName('user')
-			.setDescription('Add a user to compare against')
-	)
+  .setDescription("Displays a graph showing run history!")
+	.addStringOption(
+		new SlashCommandStringOption()
+			.setName('period')
+			.setDescription('Choose time period')
+			.setRequired(true)
+			.addChoices(
+				{ name: 'day', value: 'day' },
+				{ name: 'week', value: 'week' }
+			))
+		.addIntegerOption(
+			new SlashCommandIntegerOption()
+				.setName("count")
+				.setDescription("How many periods")
+				.setMinValue(0)
+		)
+		.addUserOption(
+			new SlashCommandUserOption()
+				.setName('user')
+				.setDescription('Add a user to compare against')
+		)
 
 export async function execute(interaction: CommandInteraction) {
-	if (!interaction.guildId) return interaction.reply(errorMessage);
-
-	const userSelect = new UserSelectMenuBuilder()
-		.setCustomId('graph-user-select')
-		.setPlaceholder(`Select user(s) for graph`)
-		.setMinValues(1)
-		.setMaxValues(4)
-
-
-	const row = new ActionRowBuilder<UserSelectMenuBuilder>()
-		.addComponents(userSelect);
-
-  return interaction.reply({
-		components: [row]
-	});
+	return interaction.reply('graph!');
 }
